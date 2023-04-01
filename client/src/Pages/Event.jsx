3f062a59eb4052061566e713b8c0d98bb87142db
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-04-01 15:03:42 +0530

    Update Event.jsx

--- client/src/Pages/Event.jsx
+++ client/src/Pages/Event.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
-import './Event.css';
-export default function Event() {
+import './Event.css'
+
+
+const Event = () => {
   return (
-    <div>
-        <div className="eventCss">
-            Event Here
-        </div>
+    <div className='wrapper'>
+        <Card img="/images/event1.png"
+            title="Event One"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex exercitationem vel excepturi mollitia molestiae aliquam officiis eligendi...."
+        />
+        <Card img="/images/event2.png"
+            title="Event Two"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex exercitationem vel excepturi mollitia molestiae aliquam officiis eligendi...."
+        />
+        <Card  img="/images/event3.png"
+            title="Event Three"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex exercitationem vel excepturi mollitia molestiae aliquam officiis eligendi...."
+        />
     </div>
   )
 }
+
+
+function Card(props) {
+    return (
+      <div className="card"> 
+       <div className='card_body'>
+          <img src={props.img} alt="image" />
+          <h2 className='card_title'> {props.title}</h2>
+          <p className='card_description'> {props.description}</p>  
+       </div>
+       <button className='card_btn'>Read More</button>
+      </div> 
+    )
+}
+
+
+export default Event;
