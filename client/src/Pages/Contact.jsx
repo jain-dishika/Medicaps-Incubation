9b1c6feb9734864746fc84a668d04b8ad17653b8
Girish Ladha <grirajladha32@gmail.com>
2023-04-04 19:11:55 +0530

    fix scroll

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -3,9 +3,9 @@ import './Contact.css';
 import ChangeComponantSelect from './ChangeContentSelect';
 export default function Contact() {
   return (
-    <div>
+    <div className='contact scroll-section' style={{paddingTop:"20vh"}}>
        
-           <div className='contactsrc'>
+           <div className='contactsrc '>
 
             {/* <h1>Contact Us</h1> */}
             <ChangeComponantSelect/>
